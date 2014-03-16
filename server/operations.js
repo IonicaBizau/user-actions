b8745e6cd1952e51197c7a03438bdf684210466e
Ionică Bizău <bizauionica@yahoo.com>
2014-03-16 19:43:35 +0200

    Added runAction operation.

---
+++ server/operations.js
@@ -0,0 +1,10 @@
+/**
+ *  user-actions#runAction
+ *
+ *  This operation runs an user action running the validations
+ *  on the server side.
+ *
+ * */
+exports.runAction = function (links) {
+    link.send(400, "Not yet implemented");
+};
