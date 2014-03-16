f8ebacf8dade6a8e32ddd51f301815981c9a10cc
Ionică Bizău <bizauionica@yahoo.com>
2014-03-16 18:04:32 +0200

    Call Events and run binds

---
+++ client/main.js
@@ -0,0 +1,30 @@
+// Bind and Events dependencies
+var Bind = require("github/jillix/bind")
+  , Events = require("github/jillix/events")
+  ;
+
+/*
+ *  This module creates the UI part for user actions in a DMS
+ *  application.
+ *
+ * */
+module.exports = function init (config) {
+
+    // get the module
+    var self = this;
+
+    // call Events
+    Events.call(self, config);
+
+    // run binds
+    if (config.binds) {
+        for (var i = 0; i < config.binds.length; ++i) {
+            Bind.call(self, config.binds[i]);
+        }
+    }
+
+    // TODO
+
+    // ready
+    self.emit("ready", config);
+};
