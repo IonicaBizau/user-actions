eb9f66901047e1b83c378265403fa3bd05651792
Ionică Bizău <bizauionica@yahoo.com>
2014-03-17 10:01:57 +0200

    Added getUserControls operation

--- server/operations.js
+++ server/operations.js
@@ -1,4 +1,41 @@
 /**
+ *
+ *  user-actions#getUserControls
+ *
+ *  TODO
+ *
+ * */
+exports.getUserControls = function (link) {
+
+    // get the data sent from client
+    var data = Object(link.data)
+      , data.filter = Object(data.filter)
+      ;
+
+    // set the _id of the current item
+    options.filter._id = String(options._id);
+
+    // create the crud object
+    var crudObject = {
+        templateId: String(data.templateId)
+      , query: data.filter
+      , role: link.session.crudRole
+      , session: link.session
+    };
+
+    // find the items via crud
+    M.emit("crud.read", crudObject, function (err, items) {
+
+        // handle error
+        if (err) {
+            return link.send(400, err);
+        }
+
+        // TODO
+    });
+};
+
+/**
  *  user-actions#runAction
  *
  *  This operation runs an user action running the validations
